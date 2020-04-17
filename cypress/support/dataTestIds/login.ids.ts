b9817595b65f69511a64516ad817ef48b2d043bd
july-nym <nym.july@gmail.com>
2020-04-17 14:05:02 +0800

    New Config #1

--- cypress/support/dataTestIds/login.ids.ts
+++
@@ -1,9 +0,0 @@
-class LoginIds {
-  public username = '#username';
-  public password ='#password';
-  public errorMsg ='.error';
-  public successMsg ='.success';
-  public logOutButton ='.button';
-}
-
-export const loginIds = new LoginIds();
--- src/slack/test/setup-tests.ts
+++
@@ -1,5 +0,0 @@
-import { config } from "dotenv";
-
-if (!process.env.CI) {
-  config({ path: "./config.env.test" });
-}
