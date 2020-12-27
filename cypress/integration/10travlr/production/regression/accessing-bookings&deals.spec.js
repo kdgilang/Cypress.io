6a7d6ffb124f497d4b8b9d9e9ee1141ea2399e23
july-nym <nym.july@gmail.com>
2020-12-27 13:36:45 +0800

    test

--- cypress/integration/10travlr/production/regression/accessing-bookings&deals.spec.js
+++ cypress/integration/10travlr/production/regression/accessing-bookings&deals.spec.js
@@ -1,92 +1,92 @@
-/// <reference types="Cypress"/>
+// /// <reference types="Cypress"/>
 
-context('Accessing My Bookings & Deals', () => {
+// context('Accessing My Bookings & Deals', () => {
     
-    describe('Visit Bookings & Deals URL', () => {
+//     describe('Visit Bookings & Deals URL', () => {
         
-        it('Checking URL', () => {
-            cy.visit('https://www.10travlr.com.au/booking-management')
+//         it('Checking URL', () => {
+//             cy.visit('https://www.10travlr.com.au/booking-management')
 
-        });
+//         });
 
-        it('Login', () => {
-            cy.get('#email').type('julian.travlr@gmail.com')
+//         it('Login', () => {
+//             cy.get('#email').type('julian.travlr@gmail.com')
     
-            cy.get('#password').type('qwertyuiop1234567890', {log:false})
+//             cy.get('#password').type('qwertyuiop1234567890', {log:false})
     
-            cy.get('#btn-submit').click()
+//             cy.get('#btn-submit').click()
 
-            cy.visit('https://www.10travlr.com.au/booking-management')
-            .location('pathname')
-            .should('eq','/booking-management')
+//             cy.visit('https://www.10travlr.com.au/booking-management')
+//             .location('pathname')
+//             .should('eq','/booking-management')
 
-        });
+//         });
 
-        it('Checking Bookings & Deals Text', () => {
-            cy.get('.block-intro-title')
-            .should('have.text', '\n                  My Bookings & Deals\n                ')
+//         it('Checking Bookings & Deals Text', () => {
+//             cy.get('.block-intro-title')
+//             .should('have.text', '\n                  My Bookings & Deals\n                ')
         
-        });
+//         });
 
-        it('Checking See All Tab', () => {
-            cy.get('#see-all > :nth-child(1) > :nth-child(1) > .block__booking__management > .empty__box > .empty__box__img')
-            .should('have.attr','src')
+//         it('Checking See All Tab', () => {
+//             cy.get('#see-all > :nth-child(1) > :nth-child(1) > .block__booking__management > .empty__box > .empty__box__img')
+//             .should('have.attr','src')
             
-        });
+//         });
 
-        it('Checking Active Logo', () => {
-            cy.get('#see-all > :nth-child(1) > :nth-child(1) > .block__booking__management > .empty__box > .empty__box__img')
-            .should('have.attr','src')
-            .and('contain','https://www.10travlr.com.au/shared/assets/images/no-transaction-img.svg')
+//         it('Checking Active Logo', () => {
+//             cy.get('#see-all > :nth-child(1) > :nth-child(1) > .block__booking__management > .empty__box > .empty__box__img')
+//             .should('have.attr','src')
+//             .and('contain','https://www.10travlr.com.au/shared/assets/images/no-transaction-img.svg')
             
-        });
+//         });
 
-        it('Checking Active & History Button', () => {
-            cy.get('.dropdown-sort-btn > .t-icon')
-            .should('be.visible')
+//         it('Checking Active & History Button', () => {
+//             cy.get('.dropdown-sort-btn > .t-icon')
+//             .should('be.visible')
 
-            cy.get('a > .toggle-cap-desktop')
-            .should('contain','History')
-            .click()
+//             cy.get('a > .toggle-cap-desktop')
+//             .should('contain','History')
+//             .click()
 
-            cy.wait(3000)
+//             cy.wait(3000)
 
-        });
+//         });
 
-        it('Login to Booking History', () => {
-            cy.get('#email').type('julian.travlr@gmail.com')
+//         it('Login to Booking History', () => {
+//             cy.get('#email').type('julian.travlr@gmail.com')
     
-            cy.get('#password').type('qwertyuiop1234567890', {log:false})
+//             cy.get('#password').type('qwertyuiop1234567890', {log:false})
     
-            cy.get('#btn-submit').click()
+//             cy.get('#btn-submit').click()
 
-            cy.visit('https://www.10travlr.com.au/booking-management?view=history')
+//             cy.visit('https://www.10travlr.com.au/booking-management?view=history')
             
-        });
+//         });
 
-        it('Checking History Logo', () => {
-            cy.get('#see-all > :nth-child(1) > :nth-child(1) > .block__booking__management > .empty__box > .empty__box__img')
-            .should('have.attr','src')
-            .and('contain','https://www.10travlr.com.au/shared/assets/images/no-transaction-img-history.svg')
+//         it('Checking History Logo', () => {
+//             cy.get('#see-all > :nth-child(1) > :nth-child(1) > .block__booking__management > .empty__box > .empty__box__img')
+//             .should('have.attr','src')
+//             .and('contain','https://www.10travlr.com.au/shared/assets/images/no-transaction-img-history.svg')
             
-        });
+//         });
 
 
-        it('Checking All Tab History', () => {
-            cy.get('.active > .nav-li-a')
-            .click()
+//         it('Checking All Tab History', () => {
+//             cy.get('.active > .nav-li-a')
+//             .click()
 
-            cy.get(':nth-child(3) > .nav-li-a')
-            .click()
+//             cy.get(':nth-child(3) > .nav-li-a')
+//             .click()
 
-            cy.get(':nth-child(4) > .nav-li-a')
-            .click()
+//             cy.get(':nth-child(4) > .nav-li-a')
+//             .click()
 
-            cy.get(':nth-child(5) > .nav-li-a')
-            .click()
+//             cy.get(':nth-child(5) > .nav-li-a')
+//             .click()
 
-        });
+//         });
 
-    });
+//     });
 
-});
+// });
--- cypress/integration/10travlr/production/regression/accessing-business-portal.spec.js
+++ cypress/integration/10travlr/production/regression/accessing-business-portal.spec.js
@@ -1,40 +1,40 @@
-/// <reference types="Cypress"/>
+// /// <reference types="Cypress"/>
 
-context('Accessing Business Portal', () => {
+// context('Accessing Business Portal', () => {
     
-    describe('Visit Business portal', () => {
+//     describe('Visit Business portal', () => {
         
-        it('Checking URL', () => {
-            cy.visit('https://www.10travlr.com.au/business_premium/test-test-test-1282692/overview')
-            .location('pathname')
-            .should('eq','/accommodation/detail/test-test-test-1282692')
+//         it('Checking URL', () => {
+//             cy.visit('https://www.10travlr.com.au/business_premium/test-test-test-1282692/overview')
+//             .location('pathname')
+//             .should('eq','/accommodation/detail/test-test-test-1282692')
         
-        });
+//         });
 
-        it('Login', () => {
-            cy.get('.mobile__header > .container-Common > .common__shortcut > .common__login > .common__login__link')
-            .click()
+//         it('Login', () => {
+//             cy.get('.mobile__header > .container-Common > .common__shortcut > .common__login > .common__login__link')
+//             .click()
 
-            cy.get('#email')
-            .type('julian.travlr@gmail.com')
+//             cy.get('#email')
+//             .type('julian.travlr@gmail.com')
     
-            cy.get('#password')
-            .type('qwertyuiop1234567890', {log:false})
+//             cy.get('#password')
+//             .type('qwertyuiop1234567890', {log:false})
     
-            cy.get('#btn-submit')
-            .click()
+//             cy.get('#btn-submit')
+//             .click()
 
-        });
+//         });
 
-        it('Visit Business Portal', () => {
-            cy.get('.mobile__header > .container-Common > .common__shortcut > .common__shortcut__profile > .dropdown > #shortcut__profile > .my__account__icon > .material-icons')
-            .click()
+//         it('Visit Business Portal', () => {
+//             cy.get('.mobile__header > .container-Common > .common__shortcut > .common__shortcut__profile > .dropdown > #shortcut__profile > .my__account__icon > .material-icons')
+//             .click()
 
-            cy.get('.mobile__header > .container-Common > .common__shortcut > .common__shortcut__profile > .dropdown > .dropdown-menu > [href="https://www.10travlr.com.au/business_premium/test-test-test-1282692/overview"]')
-            .click()
+//             cy.get('.mobile__header > .container-Common > .common__shortcut > .common__shortcut__profile > .dropdown > .dropdown-menu > [href="https://www.10travlr.com.au/business_premium/test-test-test-1282692/overview"]')
+//             .click()
             
-        });
+//         });
 
-    });
+//     });
 
-});
+// });
